a609252f87c9339af51e0194648e194945a95419
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-06-30 08:58:24 +0530

    Add on update in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,17 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className="bg-teal-800 text-white py-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold text-white">Bazaar Live</Link>
 
         <div className="hidden md:flex space-x-4 items-center">
-          <Link to="./" className="hover:text-gray-300 text-white mx-2">Home</Link>
+          <Link to="/" className="hover:text-gray-300 text-white mx-2">Home</Link>
           <Link to="/about" className="hover:text-gray-300 text-white mx-2">About Us</Link>
           <Link to="/services" className="hover:text-gray-300 text-white mx-2">Services</Link>
           <Link to="/contact" className="hover:text-gray-300 text-white mx-2">Contact</Link>
@@ -34,12 +38,12 @@ const Navbar = () => {
       {isMenuOpen && (
         <div className="md:hidden">
           <div className="flex flex-col items-center bg-teal-800 py-4">
-            <Link to="/" className="block px-4 py-2 text-sm text-white">Home</Link>
-            <Link to="/about" className="block px-4 py-2 text-sm text-white">About Us</Link>
-            <Link to="/services" className="block px-4 py-2 text-sm text-white">Services</Link>
-            <Link to="/contact" className="block px-4 py-2 text-sm text-white">Contact</Link>
-            <Link to="/login" className="block px-4 py-2 text-sm bg-gray-900 text-white rounded-md mt-4">Sign In Student</Link>
-            <Link to="/login" className="block px-4 py-2 text-sm bg-gray-900 text-white rounded-md mt-2">Sign In Teacher</Link>
+            <Link to="/" className="block px-4 py-2 text-sm text-white" onClick={handleLinkClick}>Home</Link>
+            <Link to="/about" className="block px-4 py-2 text-sm text-white" onClick={handleLinkClick}>About Us</Link>
+            <Link to="/services" className="block px-4 py-2 text-sm text-white" onClick={handleLinkClick}>Services</Link>
+            <Link to="/contact" className="block px-4 py-2 text-sm text-white" onClick={handleLinkClick}>Contact</Link>
+            <Link to="/login" className="block px-4 py-2 text-sm bg-gray-900 text-white rounded-md mt-4" onClick={handleLinkClick}>Sign In Student</Link>
+            <Link to="/login" className="block px-4 py-2 text-sm bg-gray-900 text-white rounded-md mt-2" onClick={handleLinkClick}>Sign In Teacher</Link>
           </div>
         </div>
       )}
